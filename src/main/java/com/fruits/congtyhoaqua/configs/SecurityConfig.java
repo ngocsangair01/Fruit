d0d45866c8e26f44652dd8919749711859e91b33
ngocsangair01 <ngocsangair01@gmail.com>
2022-06-16 23:51:18 +0700

    hello

--- src/main/java/com/fruits/congtyhoaqua/configs/SecurityConfig.java
+++ src/main/java/com/fruits/congtyhoaqua/configs/SecurityConfig.java
@@ -1,70 +1,70 @@
-//package com.fruits.congtyhoaqua.configs;
-//
-//import com.fruits.congtyhoaqua.filters.JwtRequestFilter;
-//import com.fruits.congtyhoaqua.services.imp.MyUserDetailService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(
-//        prePostEnabled = true,
-//        securedEnabled = true,
-//        jsr250Enabled = true)
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    private MyUserDetailService myUserDetailsService;
-//
-//    @Autowired
-//    private JwtRequestFilter jwtRequestFilter;
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
-//    }
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.cors().configurationSource(request -> corsConfiguration())
-//                .and().csrf().disable()
-//                .authorizeRequests()
-//
-//                .antMatchers("/api").permitAll();
-////                        .antMatchers("/signup").hasAnyRole("ADMIN","USER")
-////                        .antMatchers("/get-all-user").hasRole("ADMIN");
-////                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-//    }
-//    CorsConfiguration corsConfiguration() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.applyPermitDefaultValues();
-//        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
-//        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-//        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-//        return corsConfiguration;
-//    }
-//
-//    @Bean
-//    @Override
-//    protected AuthenticationManager authenticationManager() throws Exception {
-//        return super.authenticationManager();
-//    }
-//
-//}
+package com.fruits.congtyhoaqua.configs;
+
+import com.fruits.congtyhoaqua.filters.JwtRequestFilter;
+import com.fruits.congtyhoaqua.services.imp.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailService myUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().configurationSource(request -> corsConfiguration())
+                .and().csrf().disable()
+                .authorizeRequests()
+
+                .antMatchers("/api").permitAll();
+//                        .antMatchers("/signup").hasAnyRole("ADMIN","USER")
+//                        .antMatchers("/get-all-user").hasRole("ADMIN");
+//                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    CorsConfiguration corsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.applyPermitDefaultValues();
+        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        return corsConfiguration;
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+}
--- src/main/java/com/fruits/congtyhoaqua/controllers/AuthController.java
+++ src/main/java/com/fruits/congtyhoaqua/controllers/AuthController.java
@@ -1,53 +1,37 @@
-//package com.fruits.congtyhoaqua.controllers;
-//
-//import com.fruits.congtyhoaqua.bases.BaseController;
-//import com.fruits.congtyhoaqua.dtos.UserDTO;
-//import com.fruits.congtyhoaqua.filters.AuthenticationResponse;
-//import com.fruits.congtyhoaqua.models.User;
-//import com.fruits.congtyhoaqua.payload.AuthenticationRequest;
-//import com.fruits.congtyhoaqua.services.IAuthService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.validation.Valid;
-//import java.io.InvalidObjectException;
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//public class AuthController extends BaseController<AuthenticationResponse> {
-//
-//    @Autowired
-//    private IAuthService authService;
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest){
-//        return this.resSuccess(authService.login(authenticationRequest));
-//    }
-//
-//    @PostMapping("/signup-user")
-//    @PreAuthorize("hasRole('ADMIN')")
-<<<<<<< HEAD
-//    public ResponseEntity<?> signupUser(@Valid @RequestBody UserDTO userDTO){
-//        return this.resSuccess(authService.signupUser(userDTO));
-//    }
-//
-//    @PostMapping("/signup-admin")
-////    @PreAuthorize("hasRole('ADMIN')")
-//    public ResponseEntity<?> signupAdmin(@Valid @RequestBody UserDTO userDTO){
-//        return this.resSuccess(authService.signupAdmin(userDTO));
-//    }
-//
-//    @PostMapping("/validate")
-//    public ResponseEntity<?> validateToken(@RequestBody AuthenticationResponse authenticationResponse) throws InvalidObjectException {
-//        return this.resSuccess(authService.validateToken(authenticationResponse));
-//    }
-//}
-=======
+package com.fruits.congtyhoaqua.controllers;
+
+import com.fruits.congtyhoaqua.bases.BaseController;
+import com.fruits.congtyhoaqua.dtos.UserDTO;
+import com.fruits.congtyhoaqua.filters.AuthenticationResponse;
+import com.fruits.congtyhoaqua.models.User;
+import com.fruits.congtyhoaqua.payload.AuthenticationRequest;
+import com.fruits.congtyhoaqua.services.IAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.io.InvalidObjectException;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController extends BaseController<AuthenticationResponse> {
+
+    @Autowired
+    private IAuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest authenticationRequest){
+        return this.resSuccess(authService.login(authenticationRequest));
+    }
+
+    @PostMapping("/signup-user")
+    @PreAuthorize("hasRole('ADMIN')")
+
     public ResponseEntity<?> signupAdmin(@Valid @RequestBody UserDTO userDTO){
         return this.resSuccess(authService.signupAdmin(userDTO));
     }
@@ -63,4 +47,4 @@
         return this.resSuccess(authService.validateToken(authenticationResponse));
     }
 }
->>>>>>> done1
+
